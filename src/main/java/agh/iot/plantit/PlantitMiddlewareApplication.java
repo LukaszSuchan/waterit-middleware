8f8f479060dbe7ebc4246a698841f4b27877b978
Lukasz Suchan <lukasz.suchan@interia.eu>
2022-12-29 11:08:41 +0100

    init

---
+++ src/main/java/agh/iot/plantit/PlantitMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package agh.iot.plantit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantitMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantitMiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agh/iot/plantit/PlantitMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package agh.iot.plantit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantitMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
